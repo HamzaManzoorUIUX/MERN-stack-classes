6ab164ea4631409e42f8700640f0189424bee8f4
Hamza Manzoor <35131572+HamzaManzoorUIUX@users.noreply.github.com>
2023-09-20 17:39:34 +0500

    complete class 20

---
+++ class 20/src/index.ts
@@ -0,0 +1,37 @@
+{
+    const a = 60
+    let b=20
+    {
+        // const a = 50
+        console.log(a);
+        {
+            console.log(a);
+            {
+                // const a = 50
+                console.log('a', a);
+                {
+var g='ground'
+                    console.log('a', a);
+                }
+            }
+        }
+
+    }
+    const val1:string="a"
+    const val2:string="A"
+    let ab
+    console.log(ab);
+    
+    console.log(val1===val2);
+console.log(typeof(a));
+    
+}
+console.log(g);
+
+let a=123
+a=123
+a=123
+
+console.log('Hamza');
+console.warn('Hamza');
+console.error('Hamza');
