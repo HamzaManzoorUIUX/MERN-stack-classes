a70c3e2120a048f94cda30e778b28b58085c83df
Hamza Manzoor <35131572+HamzaManzoorUIUX@users.noreply.github.com>
2023-10-09 19:13:08 +0500

    class 29

---
+++ class 29/src/index.ts
@@ -0,0 +1,28 @@
+// *
+// **
+// ***
+// ****
+// *****
+
+// for (let i = 0; i < 5; i++) {
+//   let stars=""
+//     for (let j = 0; j < 5; j++) {
+//         stars+="*" //i=i+1, i+=1, 
+//     }    
+//     console.log(stars);
+// }
+
+// final 
+
+for (let i = 0; i < 5; i++) {
+    let stars=""
+      for (let j = 0; j <= i; j++) {
+          stars+="*" //i=i+1, i+=1, 
+      }    
+      console.log(stars);
+  }
+
+  for (let i = 98; i <0; i--) {
+    console.log(i)
+    
+  }
