70d0ba3f19d9414f54c106ae6f3cbf1c279d91fa
Hamza Manzoor <35131572+HamzaManzoorUIUX@users.noreply.github.com>
2024-01-15 20:09:17 +0500

    preloader done

--- whatsapp/src/components/Preloader.tsx
+++ whatsapp/src/components/Preloader.tsx
@@ -1,22 +1,49 @@
 import React from 'react';
 import styled from "styled-components";
 import { FaWhatsapp } from "react-icons/fa6";
+import ProgressBar from './ProgressBar';
+import { FaLock } from "react-icons/fa";
 const Preloader = () => {
     const PreloaderWrapper=styled.section`
     background-color:#121c21;
     color:white;
-    display:flex;
     height:100vh;
+    display:flex;
     align-items:center;
     justify-content:center;
     position:fixed;
     width:100%;
+    padding:1rem;
+    .main-content{
+        display:flex;
+        align-items:center;
+        justify-content:center;
+        flex-direction:column;
+        max-width:550px;
+        width:100%;
+        flex:1;
+    }
+    p{
+        margin-top:0px;
+    }
+    a{
+        color:white;
+        text-decoration:none;
+    }
     `
     return (
         <PreloaderWrapper>
-            <FaWhatsapp />
-            <div className='d-flex'>
-            Home
+            <div className='main-content'>
+            <FaWhatsapp style={{width:"75px",height:"75px",color:"#6c7175"}} />
+            <ProgressBar progress={20}/>
+            <h3>
+                WhatsApp
+            </h3>
+            <p>
+<FaLock/> <span>
+    End-to-end encryption. Build by <a href="http://hamzamanzoor.com" target="_blank" rel="noreferrer">Hamza Manzoor</a>
+</span>
+            </p>
             </div>
         </PreloaderWrapper>
     );
