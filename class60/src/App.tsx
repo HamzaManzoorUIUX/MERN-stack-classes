38a72c571debb7b3154144f7ae1ecfc9feef045e
Hamza Manzoor <35131572+HamzaManzoorUIUX@users.noreply.github.com>
2024-01-15 14:47:11 +0500

    custom hooks

--- class60/src/App.tsx
+++ class60/src/App.tsx
@@ -1,15 +1,18 @@
-import ButtonComp from './Component/ButtonComp';
-import MainContent from './Component/MainContent';
-import { ContextProvider } from './context/GlobalContext';
+import React from 'react';
+import useFetchApi from './useFetchApi';
 
 const App = () => {
+  const [abc,state2,url]=useFetchApi("https://dummyjson.com/products","https://dummyjson.com/products/1")
+  console.log('url',url);
+  console.log('state2',state2);
+  
+  
   return (
-    <ContextProvider>
-      <div>
-        <MainContent/>
-      </div>
-        <ButtonComp/>
-    </ContextProvider>
+    <div>
+      {
+        abc.products?.map((obj:any)=><div key={obj.id}>{obj.title}</div>)
+      }
+    </div>
   );
 };
 
