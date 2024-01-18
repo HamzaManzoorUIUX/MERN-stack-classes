b6b1bc9abf73ecfa10491cc51260fd6c0a302787
hasaigal <106329588+hasaigal@users.noreply.github.com>
2024-01-18 17:57:23 +0500

    wasi

--- whatsapp/src/App.tsx
+++ whatsapp/src/App.tsx
@@ -1,21 +1,23 @@
-import React, { useEffect, useState } from 'react';
-import Preloader from './components/Preloader';
-import MainChat from './components/MainChat';
+// import React, { useEffect, useState } from 'react';
+// import Preloader from './components/Preloader';
+// import MainChat from './components/MainChat';
+import Section from "./components/Section"
 
 const App = () => {
-  const [loading,setLoading]=useState(true)
-  const speedToHide=2
-  useEffect(()=>{
-    setTimeout(()=>{
-      setLoading(false)
-    },speedToHide*1000)
-  },[])
+  // const [loading,setLoading]=useState(true)
+  // const speedToHide=2
+  // useEffect(()=>{
+  //   setTimeout(()=>{
+  //     setLoading(false)
+  //   },speedToHide*1000)
+  // },[])
   return (
     <div>
-      {
+      <Section/>
+      {/* {
         loading?
         <Preloader speedToHide={speedToHide}/>:<MainChat/>
-      }
+      } */}
     </div>
   );
 };
