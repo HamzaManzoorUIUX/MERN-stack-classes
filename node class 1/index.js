d943bbb59deb7c096df240bb35cead0a2c517358
Hamza Manzoor <35131572+HamzaManzoorUIUX@users.noreply.github.com>
2024-01-25 20:17:35 +0500

    Update index.js

--- node class 1/index.js
+++ node class 1/index.js
@@ -1,13 +1,32 @@
-const express=require("express")
-const app=express()
+const express = require("express")
+const app = express()
 
-app.get('/',(req,res)=>{
-res.send("Hello World232")
+app.use(express.urlencoded())
+app.use(express.json())
+let users = []
+app.get('/', (req, res) => {
+    res.send(users)
 })
-app.get('/users',(req,res)=>{
-res.send("this is users api")
+app.get('/:id', (req, res) => {
+    const { id } = req.params
+    const user = users.find(obj => obj.id === id)
+    if (!user) {
+        res.status(404).send("User Not Found")
+    }
+    res.send(users.find(obj => obj.id === id))
+
+})
+app.post("/", (req, res) => {
+    const { body } = req
+    if (users.find(obj => obj.email.toLowerCase() === body.email.toLowerCase())) {
+        res.status(400).send("User Already Exists")
+    }
+    else {
+        users.push({ id: users.length.toString(), ...body, })
+        res.send(users)
+    }
 })
 
-app.listen(8000,()=>{
+app.listen(8000, () => {
     console.log("Server is running at 8000")
 })
