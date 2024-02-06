11bd7534afd8642dd7efb90a1871dcfe2f3a8599
Hamza Manzoor <35131572+HamzaManzoorUIUX@users.noreply.github.com>
2024-02-06 11:25:51 +0500

    files system

--- node class 2/src/index.ts
+++ node class 2/src/index.ts
@@ -1,8 +1,10 @@
 import express, { Express, Request, Response } from "express";
 import mariadb from 'mariadb'
+import fs from "fs";
 const app: Express = express();
 const PORT: number | string = process.env.PORT || 5000
 app.use(express.json());
+
 // this is for connection with db
 const pool = mariadb.createPool({
     host: 'localhost',
@@ -21,15 +23,15 @@ app.get("/", async (req: Request, res: Response) => {
 app.get("/:id", async (req: Request, res: Response) => {
     try {
         const connection = await pool.getConnection()
-        const { id } = req.params;    
-        
+        const { id } = req.params;
+
         if (id) {
             const response = await connection.query(`SELECT id,first_name,last_name FROM users where id='${id}'`)
             res.send(response);
         }
         // res.send('no id');
     } catch (error) {
-        
+
         res.send(error);
     }
 
@@ -38,19 +40,31 @@ app.put("/:id", async (req: Request, res: Response) => {
     try {
         const connection = await pool.getConnection()
         const { id } = req.params;
-        const {first_name,last_name} = req.body
+        const { first_name, last_name } = req.body
         if (id) {
             const response = await connection.query(`Update users set first_name='${first_name}',last_name='${last_name}' where id='${id}'`)
             res.send(response);
         }
         // res.send('no id');
     } catch (error) {
-        
+
         res.send(error);
     }
 
 });
+app.post('/api/users', async (req: Request, res: Response) => {
+    const myFile = await fs.openSync(`${__dirname}\myfiles\abc.txt`, 'a+')
+    fs.appendFile(myFile, JSON.stringify(req.body.name),(err)=>{
+        if(err){
+            res.status(500).send(err);
+        }
+        else{
+            res.status(200).send(err);
+        }
+    })
+    // res.send(JSON.stringify(req.body));
 
+})
 app.listen(PORT, () => {
     console.log(`Example app listening on port ${PORT}!`);
 });
